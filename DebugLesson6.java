0423dd5c7a8444fa8c764c98b72f833489030b8b
Qamar Yochanan <qamar.yochanan@gmail.com>
2022-11-10 14:40:54 -0600

    We actually fixed it tho :(

--- DebugLesson6.java
+++ DebugLesson6.java
@@ -4,17 +4,20 @@
 // double it every day
   // how much do you have in a 30-day month?
 public class DebugLesson6 {
-        public static void main(String args[])
-        {
-            final int DAYS = 30;
-            double money = 0.01;
-            int day = 1;
-            while(day < days)
-            {
-                money = 2 * money;
-                System.out.println("After day " + day +
-                        " you have " + money);
-            }
+
+    public static void main(String[] args) {
+
+        final int DAYS = 30;
+        double money = 0.01;
+        int day = 1;
+
+        while (day < DAYS + 1) {
+            money = 2 * money;
+            System.out.println("After day " + day +
+                    " you have " + money);
+            day++;
+
         }
     }
 }
+
