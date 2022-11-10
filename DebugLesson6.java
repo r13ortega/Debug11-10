7001d2bad228dabc20d779b44bd52524d951c44c
r13ortega <117481843+r13ortega@users.noreply.github.com>
2022-11-10 14:10:35 -0600

    Update DebugLesson6.java

--- DebugLesson6.java
+++ DebugLesson6.java
@@ -2,7 +2,7 @@
 // DebugSix1.java
 // Start with a penny
 // double it every day
-   how much do you have in a 30-day month?
+  // how much do you have in a 30-day month?
 public class DebugLesson6 {
         public static void main(String args[])
         {
